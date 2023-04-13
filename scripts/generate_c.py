205141e1fb9040cd0fa44ca2b93ef3bb9de29c63
Javier Ron Arteaga <javierron90@gmail.com>
2023-04-13 17:20:19 +0200

    added loops for programs and prompts
    Signed-off-by: Javier Ron Arteaga <javierron90@gmail.com>

--- scripts/generate_c.py
+++ scripts/generate_c.py
@@ -11,18 +11,9 @@ import re
 WORKSPACE = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 DIRNAME = os.path.abspath(os.path.dirname(__file__))
 
-def extract_substitution_from_file(file):
+def extract_source(file):
     with open(file, 'r') as file:
         data = file.read()
-
-        # lines = data.split('\n')
-
-        # for i, l in enumerate(lines):
-        #     if '<START SUBSTITUTION>' in l:
-        #         start_line = i
-        #     elif '<END SUBSTITUTION>' in l:
-        #         end_line = i
-        
         return data
 
 def parse_response(resp):
@@ -88,75 +79,52 @@ def pipeline(filepath, opt="O0"):
             new_file.write(f"{e}\n{traceback.format_exc()}")
         return (filename, None, None)
 
-def synth_substitute(substitute_code, it):
-    # with open(source_file, 'r') as file:
-    #     data = file.read()
-    #     lines = data.split('\n')
-
-    #     for i, l in enumerate(lines):
-    #         if '<START SUBSTITUTION>' in l:
-    #             start_line = i
-    #         elif '<END SUBSTITUTION>' in l:
-    #             end_line = i
-        
-    #     new = '\n'.join([
-    #         '\n'.join(lines[:start_line]), 
-    #         substitute_code,
-    #         '\n'.join(lines[end_line + 1:])
-    #     ])
-
-    with open(os.path.join(WORKSPACE, f'./rosetta_codes/quicksort.v{it}.c'), 'w') as new_file:
+def synth_substitute(filename, substitute_code, it):
+    with open(os.path.join(WORKSPACE, f'./rosetta_codes/{filename}'), 'w') as new_file:
         new_file.write(substitute_code)
 
+def main(args):
+
+    openai.api_key = open(os.path.join(DIRNAME, ".API_TOKEN")).read().strip()
+    programs = ['quicksort', 'md5' ]
+    n = 3
+
+    prompt_intros = [
+        'The following code describes an unspecified program.',
+        'I have the C code implementation in the following text'
+    ]
+    
+    prompt_instructions = [
+        'create a substitute implementation of the program, which is different but equivalent. It should be possible to directly replace the program and it should provide the same functionality.',
+        'Give me a semantically equivalent version of the program in the same language. Do not output any other text apart from the code.'
+    ]
+
+    shared_remarks = 'Do not output any other text apart from the code.'
+
+    for program in programs:
+        source_files_prefix = os.path.join(WORKSPACE, 'rosetta_codes', program)
+
+        # read from file
+        code = extract_source(f'{source_files_prefix}.c')
+        # nl_description = extract_NL(f'{source_file}.md')
     
 
-def main(args):
-    n = 100
+        for pit, prompt_tuple in enumerate(zip(prompt_intros, prompt_instructions)):
 
-    source_file = os.path.join(WORKSPACE, 'rosetta_codes', 'quicksort.c')
-    source = extract_substitution_from_file(source_file)
+            prompt = '\n'.join([prompt_tuple[0], code, prompt_tuple[1], shared_remarks])
 
-    # Read from file
-    openai.api_key = open(os.path.join(DIRNAME, ".API_TOKEN")).read().strip()
-    # algorithm = 'quicksort'
-    # n = 3
-    # language = 'golang'
+            for it in range(n):
 
-    for it in range(20, n):
+                response = openai.ChatCompletion.create(
+                    model="gpt-4",
+                    messages=[{"role": "user", "content": prompt}],
+                    temperature=1,
+                    # max_tokens=7
+                )
 
-        prompt_intro = f'The following code describes an unspecified program.'
-        prompt_intro1 = f'I have the C code implementation in the following text'
-
-        code = source
-
-        instruction = f'create a substitute implementation of the program, which is different but equivalent. It should be possible to directly replace the program and it should provide the same functionality.'
-
-        instruction1 = f'Give me a semantically equivalent version of the program in the same language. Do not output any other text apart from the code.'
-
-        # instruction_b = f'you are a software developer. You create a substitute implementation of the function `partition`. This substitute function must be different but semantically equivalent. It should be possible to directly replace the code in a program and it should provide the same functionality. Use the same function names.'
-        
-        remarks = 'Do not output any other text apart from the code.'
-        remarks1 = '''The natural language description of the programs is: Quicksort is an efficient, general-purpose sorting algorithm. Quicksort was developed by British computer scientist Tony Hoare in 1959[1] and published in 1961.[2] It is still a commonly used algorithm for sorting. Overall, it is slightly faster than merge sort and heapsort for randomized data, particularly on larger distributions.[3]
-
-            Quicksort is a divide-and-conquer algorithm. It works by selecting a 'pivot' element from the array and partitioning the other elements into two sub-arrays, according to whether they are less than or greater than the pivot. For this reason, it is sometimes called partition-exchange sort.[4] The sub-arrays are then sorted recursively. This can be done in-place, requiring small additional amounts of memory to perform the sorting.
-
-            Quicksort is a comparison sort, meaning that it can sort items of any type for which a "less-than" relation (formally, a total order) is defined. Most implementations of quicksort are not stable, meaning that the relative order of equal sort items is not preserved.
-
-        '''
-
-        prompt = '\n'.join([prompt_intro, code, instruction, remarks])
-        prompt1 = '\n'.join([prompt_intro1, code, instruction1, remarks1])
-
-        response = openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=[{"role": "user", "content": prompt1}],
-            temperature=1,
-            # max_tokens=7
-        )
-
-        # print(response)
-        parsed = parse_response(response)
-        synth_substitute(parsed, it)
+                parsed = parse_response(response)
+                new_filename = f'{program}.p{pit}.v{it}.c'
+                synth_substitute(new_filename, parsed, it)
 
 
 if __name__ == "__main__":
