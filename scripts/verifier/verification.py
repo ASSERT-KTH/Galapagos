bd1f9ba15c4c2aa847ce2db4e9c4bed37245166e
Javier Cabrera <xppcoder@gmail.com>
2023-04-21 07:30:14 +0200

    Full first alive2 verifrication

--- scripts/verifier/verification.py
+++ scripts/verifier/verification.py
@@ -3,6 +3,7 @@ import shutil
 import subprocess
 import tempfile
 import logging
+import re
 
 DIRNAME = os.path.abspath(os.path.dirname(__file__)) 
 DEBUG_FOLDER = os.path.join(DIRNAME, 'debug')
@@ -17,22 +18,76 @@ class Verifier:
         code1: file descriptor
         code2: file descriptor
 
-        returns: Custom object if the codes are equal, None otherwise
+        returns: Custom object with validation results
     '''
     def verify(code1, code2):
         raise NotImplementedError
     
 class AliveVerifier(Verifier):
 
+    class AliveResult:
+        
+        def __init__(self, original_output) -> None:
+            self.result = {}
+            self.alive_output = original_output
+            # regular expression to extract success/fail information
+            # Ex: '0 correct transformations'
+            self.alive_summary_output_regex = re.compile(r'((\d+) ((correct|incorrect|failed-to-prove) transformations|(Alive2 errors)))')
+            self._create_from_output()
+
+
+        def _create_from_output(self, ):
+            lines = self.alive_output.splitlines()
+            self.result = {}
+            for l in lines:
+                matches = self.alive_summary_output_regex.findall(l)
+                if len(matches) > 0:
+                    count = int(matches[0][1])
+                    type = matches[0][2]
+                    logging.info(f"Result matching {count} {type}")
+                    self.result[type] = count
+            # TODO parse the counter examples if errors
+            logging.info(f"{self.result}")
+            self.result
+
+        '''
+            Returns true if the verification was successful
+        '''
+        def is_ok(self):
+            return self.result['correct transformations'] > 0
+        
+        '''
+            Returns true if the verification failed
+        '''
+        def is_incorrect(self):
+            return self.result['incorrect transformations'] > 0
+
+        '''
+            Returns true if the verification failed to prove
+        '''
+        def failed_to_prove(self):
+            return self.result['failed-to-prove transformations'] > 0
+
+        '''
+            Returns true if alive2 failed
+        '''
+        def alive_failed(self):
+            return self.result['Alive2 errors'] > 0
+
     '''
         Creates an alive2 verifier instance. If debug is enabled, the verifier will save the output of alive2 to a file in the debug folder. 
         If do_function_extraction is enabled, the verifier will extract all functions from both files and will do a pairwise comparison in which the best result for verification will be returned.
     '''
     def __init__(self, debug = False, do_function_extraction = False):
+        # TODO: check alive-tv options and add them here in the constructor if needed
         self.debug = debug
         self.do_function_extraction = do_function_extraction
         self.alive_tv_bin = os.path.join(DIRNAME, "alive", "build", "alive-tv")
 
+
+    def _parse_result(self, alive_output):
+        return AliveVerifier.AliveResult(alive_output)
+    
     '''
         Receives two files and run the alive verification over them. Files should be LLVM IR codes.
     '''
@@ -68,8 +123,10 @@ class AliveVerifier(Verifier):
         alive_output = subprocess.check_output([self.alive_tv_bin, tmp1.name, tmp2.name])
         alive_output = alive_output.decode('utf-8')
         # Parse the alive output
-        
+
         logging.debug(f'Alive output: {alive_output}')
+        return self._parse_result(alive_output)
+
     
 if __name__ == "__main__":
     # setting upo logging
@@ -80,6 +137,8 @@ if __name__ == "__main__":
     code3 = open(os.path.join(DIRNAME, 'tests' ,'code3.ll'), 'r')
 
     # The one below should pass
-    v.verify(code1, code2)
+    r = v.verify(code1, code2)
+    assert r.is_ok()
     # The one below should fail
-    v.verify(code1, code3)
+    r = v.verify(code1, code3)
+    assert r.is_ok()
