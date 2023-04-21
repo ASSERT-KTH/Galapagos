efbd25a2ddaa84093eb27fcebcf95e7f8fefa16f
Javier Cabrera <xppcoder@gmail.com>
2023-04-21 11:40:25 +0200

    FIX: adding function name cleaning

--- scripts/distance_utils.py
+++ scripts/distance_utils.py
@@ -2,9 +2,10 @@ import re
 
 ABSOLUTE_ADDRESS_RE = r'^[0-9a-f]+: (([0-9a-f]){2} )+[ ]+'
 ABSOLUTE_FUNC_ADDRE = r'(0x)?[0-9a-f]+ <'
+ABSOLUTE_FUN_NAME = r'<.+?>'
 # ABSOLUTE_JMP_RE = re.compile()
 
-def clean_code(content):
+def clean_code(content, remove_function_name=True):
     # do line by line
     lines = content.split('\n')
 
@@ -13,6 +14,8 @@ def clean_code(content):
         # Remove absolute address in the start of the line
         r = re.sub(ABSOLUTE_ADDRESS_RE, '', l)
         r = re.sub(ABSOLUTE_FUNC_ADDRE, '<', r)
+        if remove_function_name:
+            r = re.sub(ABSOLUTE_FUN_NAME, '<>', r)
         # print(r)
         result += r + "\n"
 
