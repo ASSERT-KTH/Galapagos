51a681b73cde5c6059d5ed6c2a4736c224f23226
Javier Cabrera <xppcoder@gmail.com>
2023-04-28 08:44:44 +0200

    building alive script

---
+++ scripts/verifier/build.sh
@@ -0,0 +1,15 @@
+echo "Building LLVM"
+cd llvm
+mkdir build
+cd build
+cmake -GNinja -DLLVM_ENABLE_RTTI=ON -DLLVM_ENABLE_EH=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PROJECTS="llvm;clang" ../llvm
+cd ../../
+
+echo "Building alive"
+cd alive
+
+
+mkdir build
+cd build
+cmake -GNinja -DCMAKE_PREFIX_PATH=../../llvm/build -DBUILD_TV=1 -DCMAKE_BUILD_TYPE=Release ..
+ninja
