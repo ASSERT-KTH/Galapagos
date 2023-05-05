761e9347c7aa63e4188141b19e2b0f295e559b13
Javier Cabrera <xppcoder@gmail.com>
2023-05-05 12:29:41 +0200

    Better modification checking

--- scripts/usecases/case.py
+++ scripts/usecases/case.py
@@ -8,6 +8,8 @@ import shutil
 import subprocess
 import logging
 import os
+import shutil
+import  uuid
 
 '''
     Saves the file in the debug folder and returns the hash of the file
@@ -28,121 +30,57 @@ def hash_of_file(file):
 '''
 class UseCase(FileSystemEventHandler):
 
-    def __init__(self):
+    def __init__(self, debug=False):
         self.observer = None
         # The state is a map of the filepath vs hash of the file in time
         # A file changes if the last hash is != of the current hash
         # TODO move this to REDIS ?
         logging.info("Creating the empty state")
         self.state = {  }
+        self.debug = debug
 
-    '''
-        This will create a watchdog in a folder and report if something change. This will be helpful when a bitcode changes, we could map then a change from the source code to the
-        compilation without going inside the compilation process.
-        Notice that the watcher is asynchronous, so we need to use a loop to keep it running.
-    '''
-    async def create_watchdog(self, folder, check_interval=1):
-        # Fill up the state with the initial state of the folder
-        for root, dirs, files in os.walk(folder):
+
+
+    async def shadow(self, src):
+        print(f"Shadowing {src}")
+        # Create a folder in tmp
+        # Copy the src folder to the tmp folder
+        # Return the tmp folder
+        # use a random name
+
+        random_name = str(uuid.uuid4())
+        logging.info(f"Copying {src} to /tmp/{random_name}")
+        tmp_folder = os.path.join("/tmp", random_name)
+        shutil.copytree(src, tmp_folder, symlinks=False, ignore=None, copy_function=shutil.copy, ignore_dangling_symlinks=False)
+        os.sync()
+        return tmp_folder
+
+    def compare_shadows(self, src, dst):
+        # traverse root directory, and list directories as dirs and files as files
+        modified = []
+        insrc_only = []
+        indst_only = []
+
+        for root, dirs, files in os.walk(src):
             for file in files:
-                hsh = hash_of_file(os.path.join(root, file))
-                if hsh:
-                    self.state[os.path.join(root, file)] = [hsh]
+                src_file = os.path.join(root, file)
+                dst_file = os.path.join(dst, src_file[len(src)+1:])
+                if os.path.exists(dst_file):
+                    if hash_of_file(src_file) != hash_of_file(dst_file):
+                        modified.append(src_file)
+                    else:
+                        pass
+                else:
+                    insrc_only.append(src_file)
 
-        self.observer = Observer()
-        self.observer.schedule(self, folder, recursive=True)
-        self.observer.start()
-        logging.info("Starting watchdog")
+        for root, dirs, files in os.walk(dst):
+            for file in files:
+                dst_file = os.path.join(root, file)
+                src_file = os.path.join(src, dst_file[len(dst)+1:])
+                if not os.path.exists(src_file):
+                    indst_only.append(dst_file)
 
-        try:
-            while True:
-                await asyncio.sleep(check_interval)
-        except KeyboardInterrupt:
-            self.observer.stop()
-
-        self.observer.join()
-
-    def on_modified(self, event):
-        if event.is_directory:
-            return None
-        # only raise modified if the hash of the file changed !
-        hsh = hash_of_file(event.src_path)
-        if hsh and event.src_path in self.state:
-            if self.state[event.src_path][-1] != hsh:
-                self.state[event.src_path].append(hsh)
-                self.on_modified_filtered(event, hsh)
-            else:
-                # TODO replace by a logging
-                print(f"File modified but hash did not change: {event.src_path}")
-        else:
-            # Check if create is called first
-            pass
-            # create the state
-            # self.state[event.src_path] = [hsh]
-
-
-    '''
-        This is the method to be implemented by the usecase clases to filter for example, bitcodes
-    '''
-    def on_modified_filtered(self, event, hash):
-        pass
-        #print(f"File modified: {event.src_path} {hash}")
-
-
-    def on_created(self, event):
-        if event.is_directory:
-            return None
-        # TODO only if the file did not exist before in the state !
-        hsh = hash_of_file(event.src_path)
-        if hsh:
-            if event.src_path in self.state:
-                if self.state[event.src_path][-1] != hsh:
-                    self.state[event.src_path].append(hsh)
-            else:
-                self.state[event.src_path] = [hsh]
-            self.on_created_filtered(event, hsh)
-
-
-    def on_created_filtered(self, event, hash):
-        # TODO only if the file did not exist before in the state !
-        pass
-        # print(f"File created: {event.src_path} {hash}")
-
-
-
-    def on_deleted(self, event):
-        if event.is_directory:
-            return None
-
-        # remove the file from the state
-        hsh = hash_of_file(event.src_path)
-        # TODO just mark to save what happens after
-        # Do not check if exits...we do not care anyways
-        # if event.src_path in self.state:
-        #    del self.state[event.src_path]
-        if hsh:
-            self.on_deleted_filter(event, hsh)
-
-    def on_deleted_filter(self, event, hash):
-        print(f"File deleted: {event.src_path} {hash}")
-
-
-
-
-    def on_moved(self, event):
-        if event.is_directory:
-            return None
-
-        # TODO create and alias
-        hsh = hash_of_file(event.dest_path)
-        # Copy the state
-        if hsh and event.src_path in self.state:
-            self.state[event.dest_path] = self.state[event.src_path]
-            self.on_moved_filtered(event, hsh)
-
-    def on_moved_filtered(self, event, hash):
-        print(f"File moved: {event.src_path} -> {event.dest_path} {hash}")
-
+        return modified, insrc_only, indst_only
 
     '''
         Returns the files that changed.
@@ -174,17 +112,16 @@ class LLVMCompilableUseCase(UseCase):
     def __init__(self):
         super().__init__()
 
-    def compile(self):
+    async def compile(self, cwd):
 
         raise NotImplementedError()
 
 
 class TestLLVMCompilableUseCase(LLVMCompilableUseCase):
 
-    def __init__(self, lib_folder):
+    def __init__(self):
         # call constructor
         super().__init__()
-        self.lib_folder = lib_folder
         self.initial_source_code = '''
         int main() {
             return 0;
@@ -193,28 +130,26 @@ class TestLLVMCompilableUseCase(LLVMCompilableUseCase):
 
         self.changed_source_code = '''
         int main() {
-            return 3;
+            return 4;
         }
         '''
 
         self.source_code = self.initial_source_code
-
-    def compile(self):
-        print("Compiling")
-        f = open(os.path.join(self.lib_folder, "main.cpp"), "w")
+        f = open(os.path.join(os.path.dirname(__file__), "tests", "lib", "main.cpp"), "w")
         f.write(self.source_code)
+        f.close()
+
+    async def compile(self, cwd, source):
+        print(f"Compiling {cwd}")
+        f = open(os.path.join(os.path.join(cwd), "main.cpp"), "w")
+        f.write(source)
         f.close()
         # lets call clang
         # use subprocess
-        ch = subprocess.run(["clang",  "-save-temps", "-o", "main.o", "main.cpp"], cwd=self.lib_folder)
+        ch = subprocess.run(["clang",  "-save-temps", "-o", "main.o", "main.cpp"], cwd=cwd)
 
-    def set_init_state(self):
-        self.source_code = self.initial_source_code
-        self.compile()
 
-    def change_source_code(self):
-        print("Changing source code")
-        self.source_code = self.changed_source_code
+
 
 
 
@@ -229,23 +164,22 @@ if __name__ == '__main__':
 
     TEST_LIB = os.path.join(TEST_FOLDER, "lib")
 
-    uc = TestLLVMCompilableUseCase(TEST_LIB)
-    uc.set_init_state()
+    uc = TestLLVMCompilableUseCase()
+    # uc.set_init_state()
 
     async def mm():
-        t = asyncio.create_task(uc.create_watchdog(TEST_LIB))
-        await asyncio.sleep(2)
-        changed_source_codes = uc.get_last_changed()
-        # print(changed_source_codes)
-        # print([(c,uc.state[c]) for c in changed_source_codes if c.endswith(".cpp") or c.endswith(".bc") or c.endswith(".o")])
-        # uc.compile()
-        # change the source code
-        uc.change_source_code()
-        uc.compile()
-        changed_compilable = uc.get_last_changed()
 
-        print([(c, uc.state[c]) for c in changed_compilable if c.endswith(".cpp") or c.endswith(".bc") or c.endswith(".o")])
+        shadow1 = await uc.shadow(TEST_LIB)
+        shadow2 = await uc.shadow(TEST_LIB)
+
+
+        t1 = asyncio.create_task(uc.compile(shadow1, uc.initial_source_code))
+        t2 = asyncio.create_task(uc.compile(shadow2, uc.changed_source_code))
         # changed_cource_code -> changed_compilable
+        await t2
+        await t1
+        modified, insrc_only, indst_only = uc.compare_shadows(shadow1, shadow2)
+        print("Modified", modified)
 
 
     asyncio.run(mm())
