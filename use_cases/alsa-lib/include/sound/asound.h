50a38a7e70b864609deef41c9855f9c95d9ece1b
Diogo Gaspar <dgaspar@kth.se>
2024-05-20 15:11:45 +0000

    fix: alsa-lib is now a proper submodule

--- use_cases/alsa-lib/include/sound/asound.h
+++
@@ -1,4 +0,0 @@
-/* workaround for building with old glibc / kernel headers */
-#include <alsa/sound/type_compat.h>
-
-#include <alsa/sound/uapi/asound.h>
--- use_cases/alsa-lib/src/control/ctl_symbols_list.c
+++
@@ -1,3 +0,0 @@
-&_snd_module_control_remap,
-&_snd_module_control_shm,
-&_snd_module_control_ext,
