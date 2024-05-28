6fda2901b18dc4571c350c38950b46101ecafde7
Javier Ron Arteaga <javierron90@gmail.com>
2024-05-28 15:52:05 +0200

    rework code replacement for bc
    Signed-off-by: Javier Ron Arteaga <javierron90@gmail.com>

--- functions/liboqs/0_fpr_lt.c
+++ functions/liboqs/0_fpr_lt.c
@@ -1,15 +1,30 @@
-static inline int
-fpr_lt(int64_t x, int64_t y) {
+fpr_lt(fpr x, fpr y) {
+    /*
+     * If both x and y are positive, then a signed comparison yields
+     * the proper result:
+     *   - For positive values, the order is preserved.
+     *   - The sign bit is at the same place as in integers, so
+     *     sign is preserved.
+     * Moreover, we can compute [x < y] as sgn(x-y) and the computation
+     * of x-y will not overflow.
+     *
+     * If the signs differ, then sgn(x) gives the proper result.
+     *
+     * If both x and y are negative, then the order is reversed.
+     * Hence [x < y] = sgn(y-x). We must compute this separately from
+     * sgn(x-y); simply inverting sgn(x-y) would not handle the edge
+     * case x = y properly.
+     */
     int cc0, cc1;
     int64_t sx;
     int64_t sy;
 
     sx = *(int64_t *)&x;
     sy = *(int64_t *)&y;
-    sy &= ~((sx ^ sy) >> 63); 
+    sy &= ~((sx ^ sy) >> 63); /* set sy=0 if signs differ */
 
-    cc0 = (int)((sx - sy) >> 63) & 1;
-    cc1 = (int)((sy - sx) >> 63) & 1;
+    cc0 = (int)((sx - sy) >> 63) & 1; /* Neither subtraction overflows when */
+    cc1 = (int)((sy - sx) >> 63) & 1; /* the signs are the same. */
 
     return cc0 ^ ((cc0 ^ cc1) & (int)((x & y) >> 63));
 }
