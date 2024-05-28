2e72c840ab9fed6db8b9813fece54d775edd2a28
Diogo Gaspar <dgaspar@kth.se>
2024-05-28 13:45:21 +0000

    pre-processing working

---
+++ functions/ffmpeg/3_weight.c
@@ -0,0 +1,11 @@
+static int weight(int i, int blen, int offset)
+{
+
+
+
+    if (i < 2*offset)
+        return offset == 1 ? ((i) ? 5 : 3) : (1 + (6*(i) + offset - 1) / (2*offset - 1));
+    else if (i > blen-1 - 2*offset)
+        return offset == 1 ? ((blen-1 - i) ? 5 : 3) : (1 + (6*(blen-1 - i) + offset - 1) / (2*offset - 1));
+    return 8;
+}
